418ac576102fbcad86b38d5e62c81c5835cf31b0
An Ionescu <tharisfal@gmail.com>
2018-11-19 23:55:00 +0200

    Adding ValueSets and IValueSets to resolve an outstanding design problem in the entire code base. C# cannot do casts between nested interfaces unless the type is covariant, which means a lot of the code based around casting nested collection interfaces can't work. ValueSets can solve this problem, at least for MultiMap, by acting as an adapter class for the ValueCollection. More classes of this type will probably need to be added.

---
+++ Assets/Anvoker.Collections/Maps/Interfaces/IValueSets.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anvoker.Collections.Maps
+{
+    public interface IValueSets<T> : ICollection<HashSet<T>>,
+        ICollection<ICollection<T>>
+    {
+    }
+}
